3bff5f464b2098c8e16fbfe89e30ae1d1d781a57
MarianaNunez <mariana.nunez.garcia@gmail.com>
2026-03-20 22:56:01 -0700

    Implement LightsDevice class with basic functionality

---
+++ LightsDevice.cpp
@@ -0,0 +1,70 @@
+
+#include "LightsDeviceClass.h"
+
+LightsDevice::LightsDevice()
+{
+
+}
+
+LightsDevice::LightsDevice(string name, string color, int brightness)
+{
+    setName(name);
+    this -> color = color;
+    this -> brightness = brightness;
+}
+
+LightsDevice::~LightsDevice()
+{
+
+}
+
+void LightsDevice::setColor(string color)
+{
+    if (getPower() == false)
+    {
+        cout << "This device is off" << endl;
+        return;
+    }
+    decrementBattery();
+    this -> color = color;
+}
+
+string LightsDevice::getColor() const
+{
+    return this -> color;
+}
+
+void LightsDevice::setBrightness(int brightness)
+{
+    if (getPower() == false)
+    {
+        cout << "This device is off" << endl;
+        return;
+    }
+    if (brightness > 10 || brightness < 0)
+    {
+        cout << "Not a valid brightness level" << endl;
+    }
+    else
+    {
+        decrementBattery();
+        this -> brightness = brightness;
+    }
+}
+
+int LightsDevice::getBrightness() const
+{
+    return this -> brightness;
+}
+
+
+// Output to console
+void LightsDevice::displayStatus() const
+{
+    cout << "Light Device: " << getName() << endl;
+    cout << "Power: " << (getPower() ? "On" : "Off") << endl;
+    cout << "Color: " << color << endl;
+    cout << "Brightness: " << brightness << endl;
+    cout << "Battery: " << getBattery() << "%" << endl;
+}
+
