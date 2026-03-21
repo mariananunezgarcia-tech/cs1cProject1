a6227ba75170dd42c1e1fd7f97e5a8c2bdfd83f5
MarianaNunez <mariana.nunez.garcia@gmail.com>
2026-03-20 22:57:34 -0700

    Add ThermostatDevice class with temperature management

---
+++ ThermostatsDevice.h
@@ -0,0 +1,46 @@
+
+#ifndef CS1CPROJECT1_THERMOSTATDEVICE_H
+#define CS1CPROJECT1_THERMOSTATDEVICE_H
+
+
+#include "BaseDevice.h"
+
+class ThermostatDevice: public BaseDevice
+{
+public:
+    ThermostatDevice();
+    ThermostatDevice(string name, int maxTemp, int minTemp);
+    ~ThermostatDevice();
+
+    void setMaxTemp(int maxTemp);
+    int getMaxTemp();
+
+    void setMinTemp(int minTemp);
+    int getMinTemp();
+
+    int isValidTemp(int validTemp);
+
+    void setCurrentTemp(int currentTemp);
+    int getCurrentTemp();
+
+    void setTargetTemp(int targetTemp);
+    int getTargetTemp();
+
+    void setTempMode(string tempMode, int target);
+    void setTempMode(string tempMode);
+    string getTempMode();
+
+    // Newly added function
+    void displayStatus() const override;
+
+
+private:
+    int maxTemp = 100;
+    int minTemp = 0;
+    int currentTemp;
+    int targetTemp;
+    int validTemp;
+    string tempMode = "Idle";
+};
+
+#endif //CS1CPROJECT1_THERMOSTATDEVICE_H
