aed4c348543f7a45696ed1783a59ac98d4643313
Gabriel Shepardson <gshepardson0@saddleback.edu>
2026-03-23 23:31:55 -0700

    Added dynamic_casting in order to access functions for turning devices on/off, as well as accessing the battery percentage.

---
+++ DeviceManagerClassMethods.cpp
@@ -0,0 +1,104 @@
+#include "DeviceManagerClassHeader.h"
+
+// Constructor
+DeviceManager::DeviceManager()
+{
+
+}
+
+// Destructor
+DeviceManager::~DeviceManager()
+{
+
+}
+
+// Adds a new device to the manager
+void DeviceManager::addDevice(BaseDevice* device)
+{
+	devices.push_back(device); // adds the new device pointer to the vector
+}
+
+// Searches for a device by name and returns it or nullptr if not found
+BaseDevice* DeviceManager::findDevice(const string& name)
+{
+    for (int i = 0; i < devices.size(); i++)
+    {
+		// Compares the name of each device in the vector with the provided name
+        if (devices[i]->getName() == name)
+        {
+			// If a match is found, the pointer to that device is returned
+            return devices[i];
+        }
+    }
+
+	// If no device with the given name is found, a message is printed and nullptr is returned
+	cout << "Device not found." << endl;
+	return nullptr;
+}
+
+// Turns on a device by name
+void DeviceManager::turnDeviceOn(const string& name)
+{
+	// Calls findDevice to get the pointer to the device with the specified name
+    BaseDevice* device = findDevice(name);
+
+	// If the device is found the turnOn method of that device is called
+    if (device != nullptr)
+    {
+    	//Dynamically casts the found index to type DevicePowerInterface, which
+    	//has access to the turn on/off functions for the derived classes
+    	DevicePowerInterface* i = dynamic_cast<DevicePowerInterface*>(device);
+        i->turnOn();
+    }
+}
+
+// Turns off a device by name
+void DeviceManager::turnDeviceOff(const string& name)
+{
+	// Calls findDevice to get the pointer to the device with the specified name
+    BaseDevice* device = findDevice(name);
+
+	// If the device is found the turnOff method of that device is called
+    if (device != nullptr)
+    {
+    	//Dynamically casts the found index to type DevicePowerInterface, which
+    	//has access to the turn on/off functions for the derived classes
+    	DevicePowerInterface* i = dynamic_cast<DevicePowerInterface*>(device);
+        i->turnOff();
+    }
+}
+
+// Displays all devices and their status
+void DeviceManager::showAllDevices() const
+{
+    cout << "****** Smart Home Devices  ******" << endl;
+
+    for (int i = 0; i < devices.size(); i++)
+    {
+    	//Dynamically casts the found index to type DevicePowerInterface, which
+    	//has access to the turn on/off functions for the derived classes
+    	DevicePowerInterface* power = dynamic_cast<DevicePowerInterface*>(devices[i]);
+
+    	//Dynamically casts the found index to type BatteryInterface, in this
+    	//case it is not being used but is important for being able to access
+    	//the battery percentage of devices with that feature.
+    	BatteryInterface* batt = dynamic_cast<BatteryInterface*>(devices[i]);
+
+        cout << "Device Name: " << devices[i]->getName() << endl;
+        cout << "Power Status: " << (power->getPower() ? "On" : "Off") << endl;
+
+/*******************************************************************************
+ * This section is commented out because it improperly calls for a feature that
+ * is not defined in all of the derived classes.
+ ******************************************************************************/
+//        cout << "Battery: " << batt->getBattery() << "%" << endl;
+
+        cout << endl;
+    }
+}
+
+// Returns the current number of devices in the manager
+int DeviceManager::getDeviceCount() const
+{
+    return static_cast<int>(devices.size());
+}
