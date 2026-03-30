9cad8ca5346b9ff85db0738d7babac293546512c
Gabriel Shepardson <gshepardson0@saddleback.edu>
2026-03-30 16:08:15 -0700

    Added Comments

--- BaseDeviceClassMethods.cpp
+++ BaseDeviceClassMethods.cpp
@@ -1,26 +1,31 @@
 #include "BaseDeviceClassHeader.h"
 #include "ExceptionsClassHeader.h"
 
+//Default Constructor 
 BaseDevice::BaseDevice()
 {
     this -> name = "UNNAMED DEVICE";
 }
 
+//Overloaded constructor - initializes the name of the device
 BaseDevice::BaseDevice(string name)
 {
     this->name = name;
 }
 
+//Default destructor
 BaseDevice::~BaseDevice()
 {
 
 }
 
+//Method to return the name of the device
 string BaseDevice::getName() const
 {
     return this->name;
 }
 
+//Method to set the name of the device
 void BaseDevice::setName(string name)
 {
     this->name = name;
