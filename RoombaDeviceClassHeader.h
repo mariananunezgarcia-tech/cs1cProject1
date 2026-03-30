d6fa597850000b8b8d270222ac47ec093846ddb1
MarianaNunez <mariana.nunez.garcia@gmail.com>
2026-03-30 10:40:19 -0700

    Add RoombaDevice class header file

---
+++ RoombaDeviceClassHeader.h
@@ -0,0 +1,56 @@
+
+
+#ifndef CS1CPROJECT1_ROOMBADEVICECLASSHEADER_H
+#define CS1CPROJECT1_ROOMBADEVICECLASSHEADER_H
+
+#include "BaseDeviceClassHeader.h"
+#include "ExceptionsClassHeader.h"
+#include "DevicePowerInterface.h"
+#include "BatteryInterface.h"
+
+class RoombaDevice : public BaseDevice,
+                     public DevicePowerInterface,
+                     public BatteryInterface
+{
+public:
+    RoombaDevice();
+    RoombaDevice(string name, int suctionLevel);
+    ~RoombaDevice();
+
+    // virtual interface methods
+    virtual bool getPower() const;
+    virtual int getBattery() const;
+
+    virtual void turnOn();
+    virtual void turnOff();
+    virtual void setBattery(int battery);
+    virtual void decrementBattery();
+
+    virtual void displayDeviceInfo();
+
+    // roomba specific methods
+    void startCleaning();
+    void stopCleaning();
+    bool isCleaning() const;
+
+    void returnToDock();
+    bool isDocked() const;
+
+    void setSuctionLevel(int suctionLevel);
+    int getSuctionLevel() const;
+
+    void setDustBinLevel(int dustBinLevel);
+    int getDustBinLevel() const;
+    void emptyDustBin();
+
+private:
+    bool cleaning = false;
+    bool docked = true;
+    int suctionLevel = 1;
+    int dustBinLevel = 0;
+
+    bool power = false;
+    int battery = 100;
+};
+
+#endif //CS1CPROJECT1_ROOMBADEVICECLASSHEADER_H
